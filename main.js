b69a01cc304aeee998e0cad68ea9c8f7b605517b
abroraxmedov04 <abrordevaxmedov@gmail.com>
2023-10-09 17:07:22 +0300

    first-commit

---
+++ main.js
@@ -0,0 +1,350 @@
+// todo:lesson 2 homework
+// alert("Hello woerld")
+// confirm("are You sure you want to delete this message?")
+// prompt("write sth")
+
+// "use strict";
+// alert('Hello, world!');
+
+// Todo:Homework goes
+// let money = prompt("enter the amount of money that You have");
+// let dollar = 11000.34;
+// let evro = 12354.03;
+// let plane = 500;
+// let hotel = 250;
+// let museum = 120;
+// function alisherProducts() {
+//   const a = money * plane;
+//   const b = money * hotel;
+//   const c = money * museum;
+
+// }
+
+// !if 1
+// let a = prompt('enter a number')
+// if ( a > 0 ){
+//     alert ('u r on the right way')
+// } else {
+//     alert ("you are wrong ")
+// }
+
+// ! if 2
+// function ifTwo() {
+//   let a = prompt("enter a number");
+//   if (a > 0) {
+//     alert(+a + 1);
+//   } else {
+//     alert(2 + +a);
+//   }
+// }
+// ifTwo();
+
+// !if 3
+
+// function myFunction() {
+//   let number = prompt("enter a number");
+//   if (number > 0) {
+//     alert(+number + 1);
+//   } else if (number < 0) {
+//     alert(+number + 2);
+//   } else if (number == 0) {
+//     alert(10);
+//   }
+// }
+// myFunction();
+
+// !if 4
+// function myFunc(a, b, c) {
+//   if (a > 0 && b > 0 && c > 0) {
+//     console.log(`3ta musbat son bor`);
+//   } else if ((a > 0 && b > 0) || (b > 0 && c > 0) || (a > 0 && c > 0)) {
+//     console.log(`2ta musbat son bor`);
+//   } else if (a > 0 || b > 0 || c > 0) {
+//     console.log(`1ta musbat son bor`);
+//   }
+// }
+// myFunc(1, 2, 3);
+
+// !if 5
+// function myFunction() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   let c = prompt("enter the third number");
+
+// }
+// myFunction();
+// todo
+
+// ! if 6
+
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   if (a > b) {
+//         alert(a)
+//   } else if (a < b){
+//     alert(b)
+//   } else {
+//     alert("nth")
+//   }
+// }
+// myFunc();
+
+// ! if 7
+
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   if (a < b) {
+//     alert(a);
+//   } else if (b < a) {
+//     alert(b)
+//   }else{
+//     alert('n/a')
+//   }
+// }
+// myFunc();
+// TODO
+
+// ! if 8
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   if ( a == b ){
+//     console.log('the number are equal');
+//   } else if (a > b){
+// console.log(`${a} is greater than ${b}`);
+//   }else if (a < b){
+//     console.log(`${b} is greater than ${a}`);
+//   }else {
+//     console.log('numbers are not valid');
+//   }
+// }
+// myFunc();
+
+// ! if 9
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   if (a == b) {
+//     alert(+ (b + 1));
+//   } else if (a >= b) {
+//     alert(b);
+//   } else {
+//     alert("n/a");
+//   }
+// }
+
+// myFunc();
+// todo
+
+// ! if 10
+// function myFunc() {
+//   let a = +prompt("enter the first number");
+//   let b = +prompt("enter the second number");
+//   if (a !== b) {
+//     console.log(a + b);
+//   } else if (a == b) {
+//     console.log(0);
+//   } else {
+//     console.log("input invalid");
+//   }
+// }
+// myFunc();
+
+//  ! if 11
+
+// function myFunc() {
+//   let a = +prompt("enter the first number");
+//   let b = +prompt("enter the second number");
+//   if (a !== b) {
+//     console.log(a);
+//   } else if (a == b) {
+//     alert(0);
+//   } else {
+//     alert("n/t");
+//   }
+// }
+// myFunc();
+// todo
+
+//! if 12
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   let c = prompt("enter the third number");
+//   if (a < b ){
+//     alert(a)
+//   }else if (b < c){
+//     alert(b)
+//   }else if(b > c){
+//     alert(c)
+//   }else('nth')
+// }
+// myFunc();
+
+// !if 13
+
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the second number");
+//   let c = prompt("enter the third number");
+//   if (a > b || b > c) {
+//     alert(b);
+//   } else if (b > a || b > c) {
+//     alert(c);
+//   } else if (c > a || c > b) {
+//     alert(a);
+//   } else {
+//     alert("nth");
+//   }
+// }
+// myFunc();
+
+// бир иккитаси охшаш экан шу учун отип кеттим)
+
+/*------------------------------Boolean---------------------------- */
+
+// if(number % 2==0){
+
+//The number is even
+//  }
+//  else {
+
+//The number is odd
+//  }
+// ------------------------------------------------------
+// todo 1 didnt complete
+// ------------------------------------------------------
+// ! boolean 2
+// function myFunc() {
+//   let a = prompt("enter the number");
+//   if (a % 2 == 0) {
+//     alert("false");
+//   } else {
+//     alert("true");
+//   }
+// }
+// myFunc();
+
+// ! boolean 3
+// function myFunc() {
+//   let a = prompt("enter the number");
+//   if (a % 2 == 0) {
+//     alert("false");
+//   } else {
+//     alert("true");
+//   }
+// }
+// myFunc();
+
+// ! boolen 4
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the decond number");
+//   if (a > 2 && b <= 3) {
+//     alert("true");
+//   } else {
+//     alert("false");
+//   }
+// }
+// myFunc();
+
+// ! boolen 5
+// function myFunc() {
+//   let a = prompt("enter the first number");
+//   let b = prompt("enter the decond number");
+//   if (a >= 0 || b < -2) {
+//     alert("true");
+//   } else {
+//     alert("false");
+//   }
+// }
+// myFunc();
+
+// ! boolean 6
+// function myFunc() {
+//   let a = prompt("enter the number");
+//   let b = prompt("enter the number");
+//   let c = prompt("enter the number");
+//   if (a <= b <= c){
+//     alert("true");
+//   }else{
+//     alert('false');
+//   }
+// }
+// myFunc();
+
+//  ! boolean 7
+// function myFunc() {
+//   let a = prompt("enter the number");
+//   let b = prompt("enter the number");
+//   let c = prompt("enter the number");
+//   if
+// }
+
+// todo: 7 бунга лекин чунмадим
+
+//  ! boolean 8
+// function myFunc() {
+//   let a = prompt("enter the number");
+//   let b = prompt("enter the number");
+//   if (a && b % 1 == 1) {
+//     alert("true");
+//   } else {
+//     alert("false");
+//   }
+// }
+// myFunc();
+
+// ! boolen 9-10-11
+// todo: ишламадим чунки тепадаги билан бир хил экан!
+//  todo: The same method goes to them too, forgive me dont wanna  rewrite same function
+// todo: it takes my time too)
+
+// ! boolen 14
+// let a = prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number");
+// if (
+//   (a > 0 && b < 0 && c < 0) ||
+//   (a < 0 && b > 0 && c < 0) ||
+//   (a < 0 && b < 0 && c > 0)
+// ) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+// todo this one was kinda tricky hella but fine !
+
+// ! boolen 15
+// let a = prompt("enter the number");
+// let b = prompt("enter the number");
+// let c = prompt("enter the number");
+// if (
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a < 0 && b > 0 && c > 0) ||
+//   (a > 0 && b < 0 && c > 0)
+// ) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// ! boolen 16
+
+// let a = prompt("enter the number");
+// if (a < 100 && a > 9 && a % 2 == 0) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
+
+// ! boolen 17
+
+// let a = prompt("enter the number");
+// if (a < 1000 && a > 100 && a % 1 !== 1) {
+//   alert("true");
+// } else {
+//   alert("false");
+// }
